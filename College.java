c1c518ef688eae5df6aa1907268350f700be0661
manasap001 <75773687+manasap001@users.noreply.github.com>
2020-12-14 09:21:28 +0530

    Add files via upload

---
+++ College.java
@@ -0,0 +1,18 @@
+package com.xworkz.accessmodifier.protectedmodifier;
+
+public class College {
+	String name;
+	int est;
+	protected College(String name,int est)
+	{
+		name="gsssietw";
+		est=2006;
+	}
+		void teaching()
+		{
+		System.out.println("teaching ");
+    }
+		
+	
+
+}
---
+++ StudentTester.java
@@ -0,0 +1,16 @@
+package com.xworkz.accessmodifier.privatemodifier;
+
+public class StudentTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Student student=new Student("aaa","puc");
+		{
+			//student.name="aaa";
+			//student.Std="puc";
+			//student.studying();
+		}
+
+	}
+
+}

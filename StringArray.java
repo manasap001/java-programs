e72f7e0f6388392b1e27e79ba511fa558339f9ca
manasap001 <75773687+manasap001@users.noreply.github.com>
2020-12-29 09:44:40 +0530

    Add files via upload

---
+++ StringArray.java
@@ -0,0 +1,20 @@
+package com.xworkz.arrays.integer;
+
+public class StringArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1="manasa";
+		char[] ch=s1.toCharArray();
+		int len=ch.length;
+		System.out.println("char array length:"+len);
+		System.out.println("character array elements are:");
+		for(int i=0;i<ch.length;i++)
+		{
+			System.out.println(ch[i]);
+			
+
+	}
+
+}
+}
